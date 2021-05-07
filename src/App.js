54cf8e3913837a49fb49002541eadaad8e55d6ee
emil <emilbabazade29@gmail.com>
2021-05-07 18:43:00 +0300

    initial styling

--- src/App.js
+++ src/App.js
@@ -1,9 +1,44 @@
 import React from 'react'
+import {createUseStyles} from 'react-jss'
+
+const useStyles = createUseStyles({
+    container: {
+        width: '100%',
+        height: '100%',
+        backgroundColor: 'red',
+        margin: '0',
+        padding: '0'
+    },
+    content: {
+        position: 'absolute',
+        height: '100%',
+        width: '80%',
+        left: '10%',
+        right: '10%',
+    },
+    textArea: {
+        display: 'block',
+        width: '100%',
+        height: '50%',
+        margin: '0'
+    },
+    markdownArea: {
+        extend: 'textArea',
+    },
+    previewArea: {
+        extend: 'textArea',
+    }
+})
 
 const App = () => {
+    const styles = useStyles()
+
     return (
-        <div>
-      hello
+        <div className={styles.container}>
+            <div className={styles.content}>
+                <textarea className={styles.markdownArea}/>
+                <textarea className={styles.previewArea}/>
+            </div>
         </div>
     )
 }
