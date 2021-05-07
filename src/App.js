a4ad54322e6cab3470358559fee2b4e3fd048eab
emil <emilbabazade29@gmail.com>
2021-05-07 21:35:42 +0300

    can make each pane full screen

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@
   markdown - DONE
   better styling
     window title bars for editor and preview - DONE
-    button to make full screen
+    button to make full screen - DONE
     pane header font
+    animations for width change
     shadows
-  responsive
   extract into components
-  arrow images for buttons
+  stack panes vertically for mobile
+  responsive
+  arrow images for buttons - DONE
   test
 */
 
@@ -79,7 +81,8 @@ const useStyles = createUseStyles({
         border: 'solid black',
         borderWidth: '0 3px 3px 0',
         display: 'inline-block',
-        padding: '7px'
+        padding: '7px',
+        cursor: 'pointer'
     },
     rightArrow: {
         extend: 'arrow',
@@ -94,6 +97,9 @@ const useStyles = createUseStyles({
 const App = () => {
     const [editor, setEditor] = useState(initialText)
     const [preview, setPreview] = useState('')
+    const [fullScreen, setFullScreen] = useState({
+        editor: false, preview: false
+    })
 
     // initialize preview
     useEffect(() => {
@@ -111,22 +117,70 @@ const App = () => {
         // render
         setPreview(marked(cleanInput))
     }
-    
+
+    //make fullscreen on buttonpress
     const styles = useStyles()
+    const [editorButtonJssStyle, setEditorButtonStyle] = useState(styles.rightArrow)
+    const [previewButtonJssStyle, setPreviewButtonStyle] = useState(styles.leftArrow)
+
+    let editorStyle = {}
+    // let editorButtonJssStyle = styles.rightArrow
+    let previewStyle = {}
+
+    if(fullScreen.editor && !fullScreen.preview) {
+        editorStyle = {
+            width: '100%'
+        }
+        previewStyle = {
+            display: 'none'
+        }
+    }
+
+    if(fullScreen.preview && !fullScreen.editor) {
+        previewStyle = {
+            width: '100%'
+        }
+        editorStyle = {
+            display: 'none'
+        }
+    }
+    
+    const handleEditorFullScreen = () => {
+        if(!fullScreen.editor) {
+            setFullScreen({editor: true, preview: false})
+            setEditorButtonStyle(styles.leftArrow)
+        }
+        else {
+            setFullScreen({editor: false, preview: false})
+            setEditorButtonStyle(styles.rightArrow)
+        }
+    }
+
+    const handlePreviewerFullScreen = () => {
+        if(!fullScreen.preview) {
+            setFullScreen({editor: false, preview: true})
+            setPreviewButtonStyle(styles.rightArrow)
+        }
+        else {
+            setFullScreen({editor: false, preview:false})
+            setPreviewButtonStyle(styles.leftArrow)
+        }
+    }
+    
     return (
         <div className={styles.container}>
             <div className={styles.content}>
-                <div className={styles.pane}>
+                <div className={styles.pane} style={editorStyle}>
                     <div className={styles.paneHeader}>
                         <p>Editor</p>
-                        <i className={styles.rightArrow}></i>
+                        <i className={editorButtonJssStyle} onClick={handleEditorFullScreen}></i>
                     </div>
                     <textarea value={editor} onChange={handleEditorText} className={styles.markdownArea}/>
                 </div>
                 {/* already sanitized */}
-                <div className={styles.pane}>
+                <div className={styles.pane} style={previewStyle}>
                     <div className={styles.paneHeader}>
-                        <i className={styles.leftArrow}></i>
+                        <i className={previewButtonJssStyle} onClick={handlePreviewerFullScreen}></i>
                         <p>Previewer</p>
                     </div>
                     <div className={styles.previewArea} dangerouslySetInnerHTML={{
